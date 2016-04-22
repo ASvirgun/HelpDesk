92456c4a5fadb60ac54fa665cb67b9c5e5c6df5c
Alexander Svirgun <svirguna@digicode.net>
2016-04-22 12:47:51 +0300

    Add services for category, department & activ

--- HelpDeskTrain/Controllers/ServiceController.cs
+++ HelpDeskTrain/Controllers/ServiceController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using HelpDeskTrain.Models;
+using System.Data.Entity;
 
 namespace HelpDeskTrain.Controllers
 {
@@ -11,7 +12,7 @@ namespace HelpDeskTrain.Controllers
     public class ServiceController : Controller
     {
         private HelpdeskContext db = new HelpdeskContext();
-
+        #region Departments
         [HttpGet]
         public ActionResult Department()
         {
@@ -38,5 +39,68 @@ namespace HelpDeskTrain.Controllers
             db.SaveChanges();
             return RedirectToAction("Departments");
         }
+#endregion
+        #region Activ
+        [HttpGet]
+        public ActionResult Activ()
+        {
+            ViewBag.Activs = db.Activs.Include(s => s.Department);
+            ViewBag.Departments = new SelectList(db.Departments, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Activ(Activ activ)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Activs.Add(activ);
+                db.SaveChanges();
+            }
+
+            ViewBag.Activs = db.Activs.Include(s => s.Department);
+            ViewBag.Departments = new SelectList(db.Departments, "Id", "Name");
+            return View(activ);
+        }
+
+        public ActionResult DeleteActiv(int id)
+        {
+            Activ activ = db.Activs.Find(id);
+            db.Activs.Remove(activ);
+            db.SaveChanges();
+            return RedirectToAction("Activ");
+        }
+        #endregion
+
+        #region Category
+
+        [HttpGet]
+        public ActionResult Categories()
+        {
+            ViewBag.Categories = db.Categories;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Categories(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+            }
+            ViewBag.Categories = db.Categories;
+            return View(category);
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Categories");
+        }
+        #endregion
+
     }
 }

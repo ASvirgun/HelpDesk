987cfe5bb4c95f843c2eb7b85f47bec81b67b0da
Alexander Svirgun <svirguna@digicode.net>
2016-04-22 17:12:48 +0300

    add requests

---
+++ HelpDeskTrain/Controllers/RequestController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelpDeskTrain.Models;
+
+namespace HelpDeskTrain.Controllers
+{
+    [Authorize]
+    public class RequestController : Controller
+    {
+
+        HelpdeskContext db = new HelpdeskContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            User user = db.Users.FirstOrDefault(x => x.Login == HttpContext.User.Identity.Name);
+            if (user != null)
+            {
+                var cabs = from cab in db.Activs
+                    where cab.Department == user.Department
+                    select cab;
+                ViewBag.Cabs = new SelectList(cabs, "Id", "CabNumber");
+                ViewBag.Categories = new SelectList(db.Categories, "Id", "Name");
+                return View();
+            }
+            return RedirectToAction("LogOff", "Account");
+        }
+
+        [HttpPost]
+        public ActionResult Create(Request request, HttpPostedFileBase error)
+        {
+            User user = db.Users.FirstOrDefault(x => x.Login == HttpContext.User.Identity.Name);
+            if (user == null)
+            {
+                return RedirectToAction("LogOff", "Account");
+            }
+            if (ModelState.IsValid)
+            {
+                request.Status = (int)RequestStatus.Open;
+                var lifeCycle = new Lifecycle(){Opened = DateTime.Now};
+                db.Lifecycles.Add(lifeCycle);
+                request.UserId = user.Id;
+                if (error != null)
+                {
+                    string ext = error.FileName.Substring(error.FileName.LastIndexOf('.'));
+                    string path = DateTime.Now.ToString("dd/MM/yyyy H:mm:ss").Replace(":", "_").Replace("/", ".") + ext;
+                    error.SaveAs(Server.MapPath("~/Files/" + path));
+                    request.File = path;
+                }
+                db.Requests.Add(request);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(request);
+        }
+    }
+}
